05af0441324a6818d1e69ab38c0550a5712c10e3
edmundchan <pokedev8@gmail.com>
2022-11-30 23:29:05 -0800

    frontend build

---
+++ frontend/build/precache-manifest.d4476131be29844e7ad09375e2acd090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ca9d4effd3f3ed38bb7",
+    "url": "/static/css/main.5eab0312.chunk.css"
+  },
+  {
+    "revision": "2ca9d4effd3f3ed38bb7",
+    "url": "/static/js/main.2ca9d4ef.chunk.js"
+  },
+  {
+    "revision": "38ecc02d009634cff82d",
+    "url": "/static/js/1.38ecc02d.chunk.js"
+  },
+  {
+    "revision": "817c7fb6bf930b476270",
+    "url": "/static/js/2.817c7fb6.chunk.js"
+  },
+  {
+    "revision": "e3e46466794921c6291d",
+    "url": "/static/js/runtime~main.e3e46466.js"
+  },
+  {
+    "revision": "1f017a2bc66d69b6d239a6b109774b7b",
+    "url": "/index.html"
+  }
+];
