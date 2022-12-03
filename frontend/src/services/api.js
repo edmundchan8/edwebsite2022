18de1634fdfe7a1ad5446b21bacc0dddc8ba6829
edmundchan <pokedev8@gmail.com>
2022-12-02 16:17:31 -0800

    updates, does this change the .env file on frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const laravel_url = process.env.REACT_APP_BASE_URL;
+const base_url = process.env.REACT_APP_BASE_URL;
 
 const apiClient = axios.create({
-    baseURL: laravel_url,
+    baseURL: base_url,
     'Access-Control-Allow-Origin': '*',
     'Accept': 'application/json',
     withCredentials: true,
