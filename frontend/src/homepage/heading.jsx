57b29750ebab5114d6e48829712563fa407e3077
edmundchan <pokedev8@gmail.com>
2022-12-02 17:07:00 -0800

    updates

--- frontend/src/homepage/heading.jsx
+++ frontend/src/homepage/heading.jsx
@@ -9,13 +9,15 @@ function Heading(){
 
     console.log(url);
     console.log('test');
-    
+
     return(
         <div>
             <header>
                 <h1>Chi Hong Edmund Chan</h1>
                 <h3>Web Application Developer</h3>
                 <img src={profilePic} className="logo" alt="me" />
+                <h4>Test</h4>
+                <h5>{process.env.REACT_APP_BASE_URL}</h5>
             </header>   
         </div>
         
